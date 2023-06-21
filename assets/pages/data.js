e49f0847472220045af6b6a2805596082e41c075
NotAdrian99 <ruggia.adrian@gmail.com>
2023-06-21 11:03:07 -0300

    cart-branch

---
+++ assets/pages/data.js
@@ -0,0 +1,36 @@
+const data = {
+  producto: [
+    {
+      _id: "63a28d36cc6fff6724518aa3",
+      producto: "Collar de pulgas para gatos",
+      categoria: "farmacia",
+      imagen: "https://i.postimg.cc/SKCQtDDv/collar-gatos.png",
+      precio: 2000,
+      disponibles: 5,
+      descripcion:
+        "Es un antiparasitario externo a base de Clorpirifos, organofosforado que actúa por tensión de vapor con acción larvicida y adulticida Clorpirifos: Ejerce su acción, inhibiendo la acetilcolinesterasa, con lo que se mantiene la acción de la acetilcolina, neurotransmisor encargado de facilitar la comunicación neuronal y neuromuscular. La acción sostenida de la acetilcolina produce en el parásito susceptible, parálisis de tipo espástico. ",
+      __v: 0,
+    },
+    {
+      _id: "63a28d37cc6fff6724518aa5",
+      producto: "Collar de pulgas para perros",
+      categoria: "farmacia",
+      imagen: "https://i.postimg.cc/s2v7QdfZ/collar-perros.png",
+      precio: 2000,
+      disponibles: 1,
+      descripcion: "Collar apto para perros con antiparasitario externo para el control de pulgas y garrapatas, con duración de hasta 10 meses.",
+      __v: 0,
+    },
+    {
+      _id: "63a28d37cc6fff6724518aa7",
+      producto: "Cepillo",
+      categoria: "farmacia",
+      imagen: "https://i.postimg.cc/65P9WDPP/cepillo.jpg",
+      precio: 1500,
+      disponibles: 3,
+      descripcion:
+        "Complemento ideal para la limpieza y el cuidado de tu mascota. Se recomienda desenredar y cepillar el pelo de tu mascota una vez por semana, ya que previene la formación de nudos, la acumulación de suciedad y de pelo muerto. De está manera, se mantendrá limpio y brilloso por más tiempo.Beneficios :Elimina pelo muerto.Ayuda a mantener la higiene de tu mascota.Es suave al tacto. ",
+      __v: 0,
+    },
+  ],
+};
