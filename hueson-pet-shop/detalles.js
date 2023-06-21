8ac8262669ba7d9e263194a840d7c4ca4d71b06b
Samuel0273 <120742240+Samuel0273@users.noreply.github.com>
2023-06-20 21:37:02 -0500

    Add files via upload

---
+++ hueson-pet-shop/detalles.js
@@ -0,0 +1,70 @@
+const corazon = document.getElementById("corazon");
+const comprar = document.getElementById("comprar");
+const $detalleCard = document.getElementById("detalleCard");
+let colorIndex = 0;
+let textIndex = 0;
+const colors = ["#ffffff", "red"];
+const colors2 = ["#f24c3d", "#22a699"];
+const textComprar = ["Añadir al carrito", "Añadido exitosamente"];
+
+const params = new URLSearchParams(location.search);
+const id = params.get("id");
+fetch("https://mindhub-xj03.onrender.com/api/petshop")
+  .then((response) => response.json())
+  .then((data) => {
+    const ARRAY_CARRITO = [];
+    const ARRAY_FAVORITO = [];
+    corazon.addEventListener("click", () => {
+      colorIndex = (colorIndex + 1) % colors.length;
+      corazon.style.color = colors[colorIndex];
+
+      const index = ARRAY_FAVORITO.indexOf("idEncontrado");
+      if (index > -1) {
+        ARRAY_FAVORITO.splice(index, 1);
+      } else {
+        ARRAY_FAVORITO.push("idEncontrado");
+      }
+    });
+    comprar.addEventListener("click", () => {
+      colorIndex = (colorIndex + 1) % colors2.length;
+      comprar.style.backgroundColor = colors2[colorIndex];
+      textIndex = (textIndex + 1) % textComprar.length;
+      comprar.textContent = textComprar[textIndex];
+
+      const index2 = ARRAY_CARRITO.indexOf("idEncontrado");
+      if (index2 > -1) {
+        ARRAY_CARRITO.splice(index2, 1);
+      } else {
+        ARRAY_CARRITO.push("idEncontrado");
+      }
+    });
+    console.log(ARRAY_FAVORITO);
+    console.log(ARRAY_CARRITO);
+    const idEncontrado = data.find((data) => data._id == id);
+    $detalleCard.innerHTML = `<div class="card mb-3" style="max-width: 840px">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img
+            src="${idEncontrado.imagen}"
+            class="detail img-fluid rounded-start"
+            alt="..."
+          />
+        </div>
+        <div class="text col-md-8">
+          <div class="nameLike">
+            <h5 class="card-title">${idEncontrado.producto}</h5>
+            <i class="icon bi bi-heart-fill fs-2" id="corazon"></i>
+          </div>
+          <div class="info">
+            <p class="card-text" id="price">$ ${idEncontrado.precio}</p>
+            <p class="card-text" id="Disponibles">Disponibles: ${idEncontrado.disponibles}</p>
+          </div>
+          <p class="card-text" id="description">
+          ${idEncontrado.descripcion}
+          </p>
+          <button id="comprar">Añadir al carrito</button>
+        </div>
+      </div>
+    </div>`;
+  })
+  .catch((err) => console.log(err));
